17b85059513b2407b3e927d2ab1c9f125b256b58
Taufan FP Ruslanali <tfpruslanali@gmail.com>
2025-02-19 14:50:08 +0700

    merge conflict

--- sample/sampleSlice.go
+++ sample/sampleSlice.go
@@ -6,13 +6,15 @@ import (
 	"github.com/tfajarama/game-of-life/utils/print"
 )
 
-func SliceTwoDimension() {
-	slice := [][]int{{1, 0, 1}, {0, 1, 0}, {0, 0, 1}} // case 1
-	//slice := [][]int{{0, 1, 0}, {1, 0, 0}, {0, 1, 0}}	// case 2
-	//slice := [][]int{{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}} // case 3
-	print.PrintBoard(&slice)
+var (
+	slice = [][]int{{1, 0, 1}, {0, 1, 0}, {0, 0, 1}} // case 1
+	//slice = [][]int{{0, 1, 0}, {1, 0, 0}, {0, 1, 0}}	// case 2
+	//slice = [][]int{{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}} // case 3
+	banyakIterasi = 3
+)
 
-	banyakIterasi := 3
+func SliceTwoDimension() {
+	print.PrintBoard(&slice)
 
 	for k := 0; k < banyakIterasi; k++ {
 		slice = generateboard.NextBoard(&slice)
